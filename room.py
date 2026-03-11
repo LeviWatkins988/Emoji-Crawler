facd5b6612f1859f0587609d8b862fb42d256c84
WatermelonCatDev <158859953+WatermelonCatDev@users.noreply.github.com>
2026-03-10 18:45:20 -0700

    first commit

---
+++ room.py
@@ -0,0 +1,142 @@
+
+class Room():
+    def __init__(self, entered_dir, width=3, height=3):
+        # Entered_dir in left, right, up, or down
+        self.__entered_dir = entered_dir
+        self.__height = height
+        self.__width = width
+        self.__player_pos = self.find_intial_player_position()
+        self.__room = self.make_room()
+        
+
+    #Player Pos
+    @property
+    def player_pos(self):
+        return self.__player_pos
+    
+    @player_pos.setter
+    def player_pos(self, new_pos):
+        if abs(new_pos[0] - self.__player_pos[0]) > 1 or abs(new_pos[1] - self.__player_pos[1]):
+            print("Player can only move one spot")
+        elif new_pos[0] > self.__width-1 or new_pos[1] > self.__width-1:
+            print("Cannot move out of bounds")
+        else:
+            self.__player_pos = new_pos
+            self.__room = self.make_room()
+    
+
+    #Height
+    @property
+    def height(self):
+        return self.__height
+    
+    @height.setter
+    def height(self, new_height):
+        if new_height <= 0:
+            print("Height can not be 0 or negative")
+        elif new_height > 20:
+            print("Height cannot exceed 20")
+        else:
+            self.__height = new_height
+            self.__room = self.make_room()
+    
+
+    #Width
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self, new_width):
+        if new_width <= 0:
+            print("Width can not be 0 or negative")
+        elif new_width > 20:
+            print("Width cannot exceed 20")
+        else:
+            self.__width = new_width
+            self.__room = self.make_room()
+    
+
+    #Room
+    @property
+    def room(self):
+        return self.__room
+    
+    @room.setter
+    def room(self, new_room):
+        """Remakes the room based on the keys given to it"""
+        if isinstance(new_room, list) and isinstance(new_room[0], list):
+            for y in range(len(new_room)):
+                for x in range(len(new_room[y])):
+                    if new_room[y][x] == 1:
+                        self.__player_pos = (x, y)
+            self.__height = len(new_room)
+            self.__width = len(new_room[0])
+            self.__room = self.make_room()
+        else:
+            print("Room has to be a list of list with numbers to represt items in room")
+    
+    
+    #entered_dir
+    @property
+    def entered_dir(self):
+        return self.__entered_dir
+    
+    @entered_dir.setter
+    def entered_dir(self, new_dir):
+        if new_dir == "up" or new_dir == "down" or new_dir == "left" or new_dir == "right":
+            self.__entered_dir = new_dir
+            self.__player_pos = self.find_intial_player_position()
+            self.__room = self.make_room()
+        else:
+            print("Not valid direction")
+
+
+    def display(self):
+        """Displays the room"""
+        #key: white=nothing, green=player
+        for row in range(len(self.__room)):
+            for colomn in self.__room[row]:
+                if colomn == 0:
+                    print("⬜", end="")
+                elif colomn == 1:
+                    print("🟩", end="")
+            print("")
+
+    def make_room(self):
+        """makes room"""
+        bin_room = []
+        for y in range(self.__height):
+            row = []
+            for x in range(self.__width):
+                if (x, y) == self.__player_pos:
+                    row.append(1)
+                else:
+                    row.append(0)
+            bin_room.append(row)  
+        return bin_room
+    
+    def determine_middle_row(self):
+        if self.__width % 2 != 0:
+            return self.__width // 2
+        else:
+            return (self.__width // 2) - 1
+    
+    def determine_middle_column(self):
+        if self.__height % 2 != 0:
+            return self.__height // 2
+        else:
+            return (self.__height // 2) - 1
+        
+    def find_intial_player_position(self):
+        """returns a cordnate plane like position staring from the top right (x,y)"""
+        if self.__entered_dir == "up":
+            return (self.determine_middle_row(), 0)
+        elif self.__entered_dir == "down":
+            return (self.determine_middle_row(), self.__height-1)
+        elif self.__entered_dir == "left":
+            return (0, self.determine_middle_column())
+        elif self.__entered_dir == "right":
+            return (self.__width-1, self.determine_middle_column())
+
+
